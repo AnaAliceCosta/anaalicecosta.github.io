33e31a138bf27f1776f10b95cebb71cafc2b0bc3
Ana Alice <anaalicd.cd@gmail.com>
2019-07-08 16:05:04 -0300

    projeto Final

---
+++ memoria/precache-manifest.24834e61539f67ba2ca2381567600f88.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2233504707a77650868aba567a848735",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da9fd0df004bd7da29c6",
+    "url": "/static/css/main.74d4b86f.chunk.css"
+  },
+  {
+    "revision": "4b9c038b6073d6e5fd93",
+    "url": "/static/js/2.27f40313.chunk.js"
+  },
+  {
+    "revision": "da9fd0df004bd7da29c6",
+    "url": "/static/js/main.128534d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb0f19340ac99564800a331481a81e52",
+    "url": "/static/media/1.bb0f1934.jpg"
+  },
+  {
+    "revision": "8d83eedf8cbff55e5a878bd6a24669dc",
+    "url": "/static/media/10.8d83eedf.jpg"
+  },
+  {
+    "revision": "bd476560e05919836aeb612f484cb424",
+    "url": "/static/media/11.bd476560.jpg"
+  },
+  {
+    "revision": "f43a49496d51d668d5311c02e13d8671",
+    "url": "/static/media/12.f43a4949.jpg"
+  },
+  {
+    "revision": "f1ee3a739ee8b172b4f5c8043ac01045",
+    "url": "/static/media/14.f1ee3a73.jpg"
+  },
+  {
+    "revision": "50f60aed707a8aede9bfe2a2825f05fb",
+    "url": "/static/media/2.50f60aed.jpg"
+  },
+  {
+    "revision": "8d83eedf8cbff55e5a878bd6a24669dc",
+    "url": "/static/media/3.8d83eedf.jpg"
+  },
+  {
+    "revision": "bd476560e05919836aeb612f484cb424",
+    "url": "/static/media/4.bd476560.jpg"
+  },
+  {
+    "revision": "f43a49496d51d668d5311c02e13d8671",
+    "url": "/static/media/5.f43a4949.jpg"
+  },
+  {
+    "revision": "f1ee3a739ee8b172b4f5c8043ac01045",
+    "url": "/static/media/7.f1ee3a73.jpg"
+  },
+  {
+    "revision": "bb0f19340ac99564800a331481a81e52",
+    "url": "/static/media/8.bb0f1934.jpg"
+  },
+  {
+    "revision": "50f60aed707a8aede9bfe2a2825f05fb",
+    "url": "/static/media/9.50f60aed.jpg"
+  },
+  {
+    "revision": "c86e9eed74c7441240673aa7adc1cbef",
+    "url": "/static/media/back.c86e9eed.jpg"
+  }
+]);
