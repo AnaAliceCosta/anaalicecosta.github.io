86ac3947e138804481ca4592ca2e1c142585962d
Ana Alice <anaalicd.cd@gmail.com>
2019-08-14 19:41:15 -0300

    subindo listarepos

---
+++ listarepos/precache-manifest.a69def0beecf30f6aac1f1dff40d5212.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98819cc055178276410b343a60946067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9504259cfae727638a0c",
+    "url": "/static/css/main.594fdbcc.chunk.css"
+  },
+  {
+    "revision": "37e3a969b9e2e52af57e",
+    "url": "/static/js/2.78240d86.chunk.js"
+  },
+  {
+    "revision": "9504259cfae727638a0c",
+    "url": "/static/js/main.fec1719a.chunk.js"
+  },
+  {
+    "revision": "6c7ecb9b0bebebfca014",
+    "url": "/static/js/runtime~main.a665230a.js"
+  }
+]);
